c4d6b7806951e5d9b92070392f3fdef5c05d804c
zoll_h2 <nurbekerkibaev9@gmail.com>
2025-12-29 20:17:02 +0600

    The new changes in project

--- manager.py
+++ manager.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 
 def run_command(cmd: list):
+    print(f"Running command: {' '.join(cmd)}")
     subprocess.run(cmd, check=True)
 if len(sys.argv) < 2:
     print("Message: please provide a commit message.")
@@ -12,4 +13,10 @@ print("Starts working...")
 run_command(["git", "add", "."])
 run_command(["git", "commit", "-m", message])
 print("Ready to push changes.")
+print("Pushing to Github...")
+try:
+    run_command(["git", "push", "origin", "main"])
+    print("All done!")
+except subprocess.CalledProcessError as e:
+    print(f"An error occured while pusshing to Github: {e}")
 
