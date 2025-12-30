0e07f15cf4ab99ee6b09ca7a18d1fb2c73ffba7c
zoll_h2 <nurbekerkibaev9@gmail.com>
2025-12-30 08:06:34 +0600

    Feat: add Task model and in-memory db

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-app = FastAPI(title="LifeOS API", version="0.1.0")
-@app.get("/")
-async def read_root():
-    return {"system": "LifeOS", "status": "active", "version": "0.1.0"}
-@app.get("/ping")
-async def pong():
-    return {"message": "pong"}
+app = FastAPI(title="LifeOS API")
+
+class Task(BaseModel):
+    # The title of the task , which is really important
+    title:str
+    # A detailed description of the task
+    description:str | None = None
+    # The status of the task, whether it's completed or not
+    completed: bool = False
+#  WE will store tasks im memory , cause we don't have PostreSQL(for now)
+#  If the server will overload we will loose all the tasks(for now it's ok)
+fake_db = []
+#  ENDPOINTS
+@app.get("/tasks")
+async def get_tasks():
+    return fake_db
+
+# Create a new task
+# Самое интересное тут : task : Task
+# FastAPI увидит что , прочиатет json от пользователя
+# проверит его через Pydantic , и превратит в обьект task
+@app.post("/tasks")
+async def create_task(task: Task):
+    fake_db.append(task)
+    return {"status": "ok", "data": task}
